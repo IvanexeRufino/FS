21f3c7e0f396a37e95fea8c0d01b4ffd672a29e2
utnso <leandro.sanchezzarfino@gmail.com>
2016-08-30 15:56:15 -0300

    Doy formato al tp y agrego conexion mapa entrenador

---
+++ Entrenador/Debug/src/Entrenador.d
@@ -0,0 +1,1 @@
+src/Entrenador.d: ../src/Entrenador.c
---
+++ Entrenador/src/Entrenador.c
@@ -0,0 +1,111 @@
+/*
+ * Entrenador.c
+ *
+ *  Created on: 19/8/2016
+ *      Author: utnso
+ */
+
+/*
+ * Modelo ejemplo de un Cliente que envia mensajes a un Server.
+ *
+ * 	No se contemplan el manejo de errores en el sistema por una cuestion didactica. Tener en cuenta esto al desarrollar.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+
+#define IP "127.0.0.1"
+#define PUERTO "10000"
+#define PACKAGESIZE 1024	// Define cual va a ser el size maximo del paquete a enviar
+
+int main(){
+
+	/*
+	 *  ¿Quien soy? ¿Donde estoy? ¿Existo?
+	 *
+	 *  Estas y otras preguntas existenciales son resueltas getaddrinfo();
+	 *
+	 *  Obtiene los datos de la direccion de red y lo guarda en serverInfo.
+	 *
+	 */
+	struct addrinfo hints;
+	struct addrinfo *serverInfo;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;		// Permite que la maquina se encargue de verificar si usamos IPv4 o IPv6
+	hints.ai_socktype = SOCK_STREAM;	// Indica que usaremos el protocolo TCP
+
+	getaddrinfo(IP, PUERTO, &hints, &serverInfo);	// Carga en serverInfo los datos de la conexion
+
+
+	/*
+	 * 	Ya se quien y a donde me tengo que conectar... ¿Y ahora?
+	 *	Tengo que encontrar una forma por la que conectarme al server... Ya se! Un socket!
+	 *
+	 * 	Obtiene un socket (un file descriptor -todo en linux es un archivo-), utilizando la estructura serverInfo que generamos antes.
+	 *
+	 */
+	int serverSocket;
+	serverSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
+
+	int buf;
+	int i;
+	ssize_t nbytes;
+
+	/*
+	 * 	Perfecto, ya tengo el medio para conectarme (el archivo), y ya se lo pedi al sistema.
+	 * 	Ahora me conecto!
+	 *
+	 */
+	connect(serverSocket, serverInfo->ai_addr, serverInfo->ai_addrlen);
+	freeaddrinfo(serverInfo);	// No lo necesitamos mas
+
+	/*
+	 *	Estoy conectado! Ya solo me queda una cosa:
+	 *
+	 *	Enviar datos!
+	 *
+	 *	Vamos a crear un paquete (en este caso solo un conjunto de caracteres) de size PACKAGESIZE, que le enviare al servidor.
+	 *
+	 *	Aprovechando el standard immput/output, guardamos en el paquete las cosas que ingrese el usuario en la consola.
+	 *	Ademas, contamos con la verificacion de que el usuario escriba "exit" para dejar de transmitir.
+	 *
+	 */
+	int enviar = 1;
+	char message[PACKAGESIZE];
+
+	printf("Conectado al servidor. Bienvenido al sistema, ya puede enviar mensajes. Escriba 'exit' para salir\n");
+
+	while(enviar){
+		fgets(message, PACKAGESIZE, stdin);			// Lee una linea en el stdin (lo que escribimos en la consola) hasta encontrar un \n (y lo incluye) o llegar a PACKAGESIZE.
+		if (!strcmp(message,"exit\n"))
+			{
+			enviar = 0;			// Chequeo que el usuario no quiera salir
+			}
+		if (enviar)
+			{
+			send(serverSocket, message, strlen(message) + 1, 0); 	// Solo envio si el usuario no quiere salir.
+			}
+	}
+
+
+
+
+	/*
+	 *	Listo! Cree un medio de comunicacion con el servidor, me conecte con y le envie cosas...
+	 *
+	 *	...Pero me aburri. Era de esperarse, ¿No?
+	 *
+	 *	Asique ahora solo me queda cerrar la conexion con un close();
+	 */
+
+	close(serverSocket);
+	return 0;
+
+	/* ADIO'! */
+}
