f3e6ce3c08acf90644becb080d4f8fb7b0a290ba
sora797 <natitorokian@gmail.com>
2016-09-10 01:00:13 -0300

    logger osada

--- PokedexServidor/src/FileSysOSADA/osada.c
+++ PokedexServidor/src/FileSysOSADA/osada.c
@@ -1,22 +1,32 @@
 #include "osada.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <commons/log.h>
+
+#define LOG_FILE "osada.log"
+#define PROGRAM_NAME "Pokedex Servidor"
+#define PROGRAM_DESCRIPTION "Proceso File System"
+#define IS_ACTIVE_CONSOLE true
+#define T_LOG_LEVEL LOG_LEVEL_INFO
 
 
 int main (void) {
 
 	FILE *archivo;
 	osada_header *header;
+	t_log* logger;
+	logger = log_create(LOG_FILE, PROGRAM_NAME, IS_ACTIVE_CONSOLE, T_LOG_LEVEL);
+	log_info(logger, PROGRAM_DESCRIPTION);
 
-	archivo = fopen("disco.bin","r");
+	archivo = fopen("./osada-format disco.bin","r");
 	fread(header,sizeof(osada_header),1,archivo);
-	printf("Identificador (bytes): %s\n",header->magic_number);
-	printf("Version: %d\n",header->version);
-	printf("Tamaño FS: %d\n",header->fs_blocks);
-	printf("Tamaño Bitmap: %d\n",header->bitmap_blocks);
-	printf("Inicio tabla asignaciones: %d\n",header->allocations_table_offset);
-	printf("Tamaño datos: %d\n",header->data_blocks);
-	printf("Relleno: %d\n\n",header->padding);
+	log_info(logger, printf("Identificador (bytes): %s\n",header->magic_number));
+	log_info(logger, printf("Version: %d\n",header->version));
+	log_info(logger, printf("Tamaño FS: %d\n",header->fs_blocks));
+	log_info(logger, printf("Tamaño Bitmap: %d\n",header->bitmap_blocks));
+	log_info(logger, printf("Inicio tabla asignaciones: %d\n",header->allocations_table_offset));
+	log_info(logger, printf("Tamaño datos: %d\n",header->data_blocks));
+	log_info(logger, printf("Relleno: %d\n\n",header->padding));
 
 	fclose(archivo);
 
