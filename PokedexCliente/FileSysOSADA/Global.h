89c2d0eed1fbb03e236443f9bf3123cb11b383b9
ivanexerufino <ivan-2100@hotmail.com>
2016-10-25 20:38:33 -0300

    moviendo fileSys al cliente

---
+++ PokedexCliente/FileSysOSADA/Global.h
@@ -0,0 +1,25 @@
+/*
+ * Global.h
+ *
+ *  Created on: 20/9/2016
+ *      Author: utnso
+ */
+
+#ifndef POKEDEXSERVIDOR_SRC_FILESYSOSADA_GLOBAL_H_
+#define POKEDEXSERVIDOR_SRC_FILESYSOSADA_GLOBAL_H_
+
+#include "osada.h"
+#include <commons/bitarray.h>
+
+char * disco;
+osada_header* header;
+t_bitarray* bitmap;
+osada_file* tablaDeArchivos;
+osada_block_pointer* tablaDeAsignaciones;
+osada_block* bloquesDeDatos;
+int inicioDeBloqueDeDatos;
+int cantidadDeBloques;
+
+
+
+#endif /* POKEDEXSERVIDOR_SRC_FILESYSOSADA_GLOBAL_H_ */
