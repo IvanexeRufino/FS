4ca8b87c7eeaa8ae6163e03c6d03ef250cfaa8ac
ivanexerufino <ivanexerufino>
2016-11-18 19:06:40 -0300

    pokedexCliente peqeño cambio

--- PokedexServidor/src/PokedexServidor.c
+++ PokedexServidor/src/PokedexServidor.c
@@ -53,6 +53,76 @@ t_paquete* desacoplador(char* buffer)/*transforma multiples streams en estructur
   	return paquete;
   }
 
+int crearSocketServidor(char *puerto) {
+	int BACKLOOG = 5;
+	struct addrinfo hints;
+	struct addrinfo* serverInfo;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_flags = AI_PASSIVE;
+	hints.ai_socktype = SOCK_STREAM;
+	getaddrinfo(NULL, puerto, &hints, &serverInfo);
+	int listenningSocket;
+	listenningSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype,
+			serverInfo->ai_protocol);
+	int yes =1;
+	setsockopt(listenningSocket,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int));
+	bind(listenningSocket, serverInfo->ai_addr, serverInfo->ai_addrlen);
+	freeaddrinfo(serverInfo);
+
+	listen(listenningSocket, BACKLOOG);
+	return listenningSocket;
+}
+
+int IniciarSocketServidor(int puertoServer)
+{
+	struct sockaddr_in socketInfo;
+		int socketEscucha;
+		int optval = 1;
+
+		// Crear un socket
+		socketEscucha = socket (AF_INET, SOCK_STREAM, 0);
+		if (socketEscucha == -1)
+		 	return -1;
+
+		setsockopt(socketEscucha, SOL_SOCKET, SO_REUSEADDR, &optval,
+				sizeof(optval));
+		socketInfo.sin_family = AF_INET;
+		socketInfo.sin_port = htons(puertoServer);
+		socketInfo.sin_addr.s_addr = INADDR_ANY;
+		if (bind (socketEscucha,(struct sockaddr *)&socketInfo,sizeof (socketInfo)) != 0)
+		{
+			close (socketEscucha);
+			return -1;
+		}
+
+		/*
+		* Se avisa al sistema que comience a atender llamadas de clientes
+		*/
+		if (listen (socketEscucha, 10) == -1)
+		{
+			close (socketEscucha);
+			return -1;
+		}
+		/*
+		* Se devuelve el descriptor del socket servidor
+		*/
+		return socketEscucha;
+}
+
+int AceptarConexionCliente(int socketServer) {
+	socklen_t longitudCliente;//esta variable tiene inicialmente el tamaño de la estructura cliente que se le pase
+	struct sockaddr cliente;
+	int socketNuevaConexion;//esta variable va a tener la descripcion del nuevo socket que estaria creando
+	longitudCliente = sizeof(cliente);
+	socketNuevaConexion = accept (socketServer, &cliente, &longitudCliente);//acepto la conexion del cliente
+	if (socketNuevaConexion < 0){
+		return -1;
+	}
+	return socketNuevaConexion;
+
+}
+
 void recibirQueSos(int newfd){
 	char* buforecibidox;
 	char* buforecibido;
@@ -198,8 +268,8 @@ void sigchld_handler(int s){
 
 int main(void) {
 
-//	system("truncate -s 1024k disco.bin");
-//	system("./osada-format disco.bin");
+	system("truncate -s 100k disco.bin");
+	system("./osada-format disco.bin");
 	reconocerOSADA("disco.bin");
 
 	int sockfd, new_fd;  // Escuchar sobre sock_fd, nuevas conexiones sobre new_fd
