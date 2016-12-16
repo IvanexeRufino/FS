9f942ed883e819f9753965b780e5b9071a681c34
GabiAlifraco <gabriel.alifraco@gmail.com>
2016-12-16 16:09:19 -0300

    Arreglando los makefiles

--- PokedexCliente/Debug/src/PokedexCliente.d
+++
@@ -1,6 +0,0 @@
-src/PokedexCliente.d: ../src/PokedexCliente.c ../src/pokedexcliente.h \
- ../src/FileSysOSADA/osada.h
-
-../src/pokedexcliente.h:
-
-../src/FileSysOSADA/osada.h:
--- PokedexServidor/Debug/src/PokedexServidor.d
+++
@@ -1,6 +0,0 @@
-src/PokedexServidor.d: ../src/PokedexServidor.c ../src/pokedexservidor.h \
- ../src/FileSysOSADA/osada.h
-
-../src/pokedexservidor.h:
-
-../src/FileSysOSADA/osada.h:
